82faf5f5e19545368835e2dd66fc594bbf247204
Joshua Summers <J.Eichler-Summers@sms.ed.ac.uk>
2019-11-15 15:00:47 +0000

    latest app

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -2,30 +2,42 @@ class DosesController < ApplicationController
   def create
 
     # to do, route with variable not params after error?
+    # stop auto complete on forms
     # alphabetical ingredients
     # ajax / not to reload
 
     @dose = Dose.new(dose_params)
     @cocktail_w_ingredient = Cocktail.find(params[:cocktail_id])
-    @ingredient = Ingredient.find(params[:dose][:ingredient])
     @dose.cocktail = @cocktail_w_ingredient
-    @dose.ingredient = @ingredient
+    # @dose.ingredient = @ingredient
 
     if @dose.save
+      # flash[:error] = 'SUPER ERRROR'
       redirect_to root_path,
-      notice: "#{@dose.description} of #{@ingredient.name} added to #{@cocktail_w_ingredient.name}"
+      notice: "#{@dose.description} of #{@dose.ingredient.name} added to #{@cocktail_w_ingredient.name}"
     else
-      ingredient_error_msg = "#{@ingredient.name} NOT added to #{@cocktail_w_ingredient.name}. No description given."
+      # flash[:error] = 'SUPER ERRROR'
+      # flash[:success] = 'SUPER ERRROR'
+      # flash[:notice] = 'SUPER ERRROR'
+      # flashp[:hello_anything_else] = 'hello'
+      # uses same flash keys... as bootstrap, warning, error, success
+      # so if have flash
+      ingredient_error_msg = "#{@dose.ingredient.name} NOT added to #{@cocktail_w_ingredient.name}. No description given."
       redirect_to root_path(ingredient_error_msg: ingredient_error_msg)
       # index_instance_variables
       # render template: 'cocktails/index'#,
+      # these are called flash messages
+      # notice... he does not use
+      # its new
+      # set a flash something, make new method
+
     end
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 
   def index_instance_variables
